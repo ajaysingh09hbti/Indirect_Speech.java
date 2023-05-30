39292b0af74e5ac860370e31a2cd95eb4e332fc3
ajaysingh09hbti <education.me90@gmail.com>
2023-05-30 22:01:17 +0530

    Initial commit for indirect speech upload

---
+++ src/Indirect_Speech.java
@@ -0,0 +1,10 @@
+public class Indirect_Speech {
+    public static void main ( String [] args ){
+
+        System.out.println("Today we are going to discuss about indirect speech....");
+
+        System.out.println("narration has two types.........");
+
+        System.out.println("Direct speech and indirect speech.....");
+    }
+}
